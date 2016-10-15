8ac86055402f8203f6c92e19eb9991d4dc7f5670
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2016-10-14 21:22:34 -0700

    Update TPCH_installer.sh

--- azure/TPCH_installer.sh
+++ azure/TPCH_installer.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
 
-if [[ `hostname -f` -z `get_primary_headnode` ]]; then
+if [[ `hostname -f` == `get_primary_headnode` ]]; then
 	wget https://github.com/dharmeshkakadia/tpch-datagen-as-hive-query/archive/master.zip
 	unzip master.zip; cd tpch-datagen-as-hive-query-master;
 	hive -i settings.hql -f TPCHDataGen.hql -hiveconf SCALE=10 -hiveconf PARTS=$1 -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf TPCHBIN=resources 
