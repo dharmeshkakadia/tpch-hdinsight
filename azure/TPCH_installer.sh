a3263b0df3fd41fe75576a1222bb0fd1b87bd936
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2016-10-14 20:06:18 -0700

    Update TPCH_installer.sh

--- azure/TPCH_installer.sh
+++ azure/TPCH_installer.sh
@@ -5,5 +5,5 @@ if [[ `hostname -f` -z `get_primary_headnode` ]]; then
 	unzip master.zip; cd tpch-datagen-as-hive-query-master;
 	hive -i settings.hql -f TPCHDataGen.hql -hiveconf SCALE=10 -hiveconf PARTS=$1 -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf TPCHBIN=resources 
 	hive -i settings.hql -f ddl/createAllExternalTables.hql -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf DBNAME=tpch
-	hive -i settings.hql -f ddl/createAllORCTables.hql -hiveconf ORCDBNAME=$2 -hiveconf SOURCE=tpch 
+	hive -i settings.hql -f ddl/createAllORCTables.hql -hiveconf ORCDBNAME=tpch_orc -hiveconf SOURCE=tpch 
 fi
