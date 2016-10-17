c07011dcfb35b2e8c7f8cae4326968fc41d99385
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2016-10-17 11:10:08 -0700

    Update TPCH_installer.sh

--- azure/TPCH_installer.sh
+++ azure/TPCH_installer.sh
@@ -5,5 +5,7 @@ wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.win
 if [[ `hostname -f` == `get_primary_headnode` ]]; then
 	wget https://github.com/dharmeshkakadia/tpch-datagen-as-hive-query/archive/master.zip
 	unzip master.zip; cd tpch-datagen-as-hive-query-master;
-	nohup bash 'hive -i settings.hql -f TPCHDataGen.hql -hiveconf SCALE=10 -hiveconf PARTS=$1 -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf TPCHBIN=resources && hive -i settings.hql -f ddl/createAllExternalTables.hql -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf DBNAME=tpch && hive -i settings.hql -f ddl/createAllORCTables.hql -hiveconf ORCDBNAME=tpch_orc -hiveconf SOURCE=tpch' &
+	hive -i settings.hql -f TPCHDataGen.hql -hiveconf SCALE=10 -hiveconf PARTS=$1 -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf TPCHBIN=resources
+	hive -i settings.hql -f ddl/createAllExternalTables.hql -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf DBNAME=tpch
+	nohup hive -i settings.hql -f ddl/createAllORCTables.hql -hiveconf ORCDBNAME=tpch_orc -hiveconf SOURCE=tpch' &
 fi
